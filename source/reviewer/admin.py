83b9b64a87e3baf1fc69fe54108131f246efd114
tilekbai <kukushokk@gmail.com>
2021-05-01 11:56:21 +0600

    Создана модель Good в моделях и админ панели

--- source/reviewer/admin.py
+++ source/reviewer/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Good
 # Register your models here.
+
+class GoodAdmin(admin.ModelAdmin):
+    list_display = ["id", "summary", "description", "category", "picture"]
+    list_filter = ["id", "summary", "category"]
+    search_fields = ["description", "summary", "category"]
+    fields = ["id", "summary", "description", "category", "picture"]
+    readonly_fields = ["id"]
+
+admin.site.register(Good, GoodAdmin)
--- source/reviewer/models.py
+++ source/reviewer/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Good(models.Model):
+    CHOICES = [("1", "food"), ("2", "education"), ("3", "clothes"), ("4", "electronic")]
+    summary = models.CharField(max_length = 60, null = False, blank = False, verbose_name = "Заголовок")
+    description = models.CharField(max_length = 2000, null = True, blank=True, verbose_name = "Описание")
+    category = models.CharField(max_length = 29, null = True, blank=False, verbose_name = "Категория", choices= CHOICES)
+    picture = models.ImageField(blank=True)
+
+    class Meta:
+        db_table = "goods"
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
